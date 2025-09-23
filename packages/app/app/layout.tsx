4de3209e335ed6432c98025e7e52cdf489a6b8aa
Filip Melka <melka.filip@proton.me>
2025-09-23 17:22:41 +0200

    chore: install prettier

--- packages/app/app/layout.tsx
+++ packages/app/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Sharesnap",
-  description: "Quickly share pictures from phone to desktop",
-};
+  title: 'Sharesnap',
+  description: 'Quickly share pictures from phone to desktop',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
