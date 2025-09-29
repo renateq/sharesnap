69110fd3671638b9d10ef71a5d19e4670167808b
Filip Melka <melka.filip@proton.me>
2025-09-29 10:59:36 +0200

    feat: UI refinements

--- packages/app/app/hero.tsx
+++ packages/app/app/hero.tsx
@@ -1,30 +1,30 @@
 import { Rabbit, ShieldCheck, Sparkles } from 'lucide-react'
 import { QRCode } from './qr-code'
 
-const iconSize = 20
+const iconSize = 25
 
 function Features() {
   const data = [
     {
-      icon: <Sparkles size={iconSize} />,
-      label: 'Simple',
-    },
-    {
       icon: <Rabbit size={iconSize} />,
-      label: 'Performant',
+      label: 'Fast',
     },
     {
       icon: <ShieldCheck size={iconSize} />,
       label: 'Secure',
     },
+    {
+      icon: <Sparkles size={iconSize} />,
+      label: 'Zero Setup',
+    },
   ]
 
   return (
-    <div className="flex justify-center gap-5 opacity-60">
+    <div className="flex justify-center gap-6 opacity-60">
       {data.map(({ icon, label }) => (
-        <div className="flex items-center gap-1" key={label}>
+        <div className="flex items-center gap-2" key={label}>
           {icon}
-          <span className="text-lg font-medium">{label}</span>
+          <span className="text-xl font-medium">{label}</span>
         </div>
       ))}
     </div>
@@ -33,24 +33,26 @@ function Features() {
 
 function Title() {
   return (
-    <h2 className="text-center text-4xl font-semibold">Simply scan and snap</h2>
+    <h2 className="text-center text-4xl leading-12 font-semibold">
+      Send photos to your
+      <br />
+      laptop in seconds.
+    </h2>
   )
 }
 
 export function Hero() {
   return (
-    <div className="flex min-h-[calc(100dvh-5rem)] flex-col justify-center gap-16">
-      <div className="flex flex-col gap-4">
+    <div className="min-h-[calc(100dvh-5rem)]">
+      <div className="flex flex-col gap-4 pt-12">
         <Features />
-
         <Title />
       </div>
-      <div className="my-16 flex justify-center">
+      <div className="mt-20 mb-32 flex justify-center">
         <QRCode />
       </div>
-
-      <p className="mx-auto max-w-xs pb-52 text-center opacity-70">
-        The simplest way to transfer images from your phone to your desktop.
+      <p className="mx-auto max-w-sm pb-20 text-center text-lg opacity-70">
+        No emails. No cables. Just scan and send.
       </p>
     </div>
   )
